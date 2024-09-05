ec84fb05042034bface28256dd53e78caf33932b
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-09-05 12:32:03 +0700

    fix(hero): :lipstick: Fix styling for hero section

--- src/components/Pages/Home/Hero.tsx
+++ src/components/Pages/Home/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Hero(): React.ReactElement {
   return (
-    <div className="relative h-[700px] flex flex-col justify-center">
+    <div className="relative h-[800px] flex flex-col justify-center">
       <div className="absolute -z-[1] top-0 left-10 -translate-x-1/2 -translate-y-1/2 max-w-[600px]">
         <Image
           src={"/img/blobs1.svg"}
