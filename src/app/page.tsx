f934c694af6434d4cf14eca2316a2a3a54bbef3b
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-09-05 11:41:28 +0700

    feat(about): :sparkles: Create about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,11 @@
-import Button from "@/components/Button";
-import Typography from "@/components/Typography";
-import Image from "next/image";
+import About from "@/components/Pages/Home/About";
+import Hero from "@/components/Pages/Home/Hero";
 
-export default function Home() {
+export default function Home(): React.ReactElement {
   return (
-    <div className="relative h-[700px] flex flex-col justify-center">
-      <div className="absolute -z-[1] top-0 left-10 -translate-x-1/2 -translate-y-1/2 max-w-[600px]">
-        <Image
-          src={"/img/blobs1.svg"}
-          alt="blobs"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-        />
-      </div>
-      <div className="absolute -z-[1] bottom-0 right-0 translate-y-1/2 rotate-60 max-w-[300px] md:max-w-[400px]s">
-        <Image
-          src={"/img/blobs2.svg"}
-          alt="blobs"
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-        />
-      </div>
-      <div className="container px-6 flex flex-col gap-4">
-        <Typography.Heading
-          level={1}
-          className="!text-4xl !font-semibold text-center"
-        >
-          Manage Your Business Efficiently
-          <br />
-          with Our ERP System
-        </Typography.Heading>
-        <Typography.Paragraph size="lg" className="text-center">
-          Track your inventory, monitor your sales, and maximize your profits
-          with ease.
-        </Typography.Paragraph>
-        <div className="flex justify-center">
-          <Button>Get Started</Button>
-        </div>
-        <div className="flex justify-center max-w-[350px] mx-auto">
-          <Image
-            src={"/img/jumbotron-img.svg"}
-            alt="jumbotron"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: "100%", height: "auto" }}
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <Hero />
+      <About />
+    </>
   );
 }
---
+++ src/components/Pages/Home/About.tsx
@@ -0,0 +1,50 @@
+import Typography from "@/components/Typography";
+import Image from "next/image";
+
+export default function About(): React.ReactElement {
+  return (
+    <div className="relative">
+      <div className="md:block hidden absolute -z-[1] top-0 xl:left-[200px] lg:left-[130px] md:left-[75px] -translate-x-1/2 max-w-[600px] -rotate-[3deg]">
+        <Image
+          src={"/img/blobs3.svg"}
+          alt="blobs"
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: "100%", height: "auto" }}
+        />
+      </div>
+      <div className="container md:grid grid-cols-3 items-center min-h-[500px]">
+        <div className="col-span-1 flex flex-col justify-center">
+          <div className="flex justify-center">
+            <Typography.Heading
+              level={2}
+              className="!text-4xl md:text-slate-50"
+            >
+              About <br className="md:block hidden" />
+              ProfitFlow
+            </Typography.Heading>
+          </div>
+        </div>
+        <div className="col-span-2 max-w-[630px] mx-auto md:pt-24 pt-8">
+          <Typography.Paragraph size="lg" className="!font-semibold">
+            Our ERP system is designed to help business owners streamline their
+            operations, manage inventory efficiently, and maximize profits. With
+            our user-friendly interface, businesses of all sizes can benefit
+            from the powerful features we offer
+          </Typography.Paragraph>
+          <div className="mx-auto max-w-[400px] mt-6">
+            <Image
+              src={"/img/about-img.svg"}
+              alt="about illustration"
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
